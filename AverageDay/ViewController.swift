854b8b67f518027fbea0d21f2bc37922786d65fc
J.Manuel Zaragoza <zaragozajn@gmail.com>
2022-04-24 16:13:44 -0700

    Initial Commit

---
+++ AverageDay/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AverageDay
+//
+//  Created by J Manuel Zaragoza on 4/24/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
